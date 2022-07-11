891249af22b2cbec884a37dd5a2903254ac870ff
“Dylan <dylan050603@gmail.com>
2022-07-11 15:38:09 +0200

    mise à jour du temps pour faire tourner la roue

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-#imports
+# imports
 import json, JSONS, GUI
 from tkinter import *
 from tkinter import ttk
+
 url = 'things.json'
 objects = JSONS.read(url)
-print(objects['idees'])
 GUI.window()
-
